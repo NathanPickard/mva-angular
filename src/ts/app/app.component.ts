63282bedd917d477aaca0496df748072443286d3
Nathan Pickard <nathan.pickard87@gmail.com>
2018-02-01 22:15:54 -0800

    Form input, styles, and keyboard events

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,10 +2,22 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    template: require("./app.component.html"),
+    styles: [require("./app.component.scss")]
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public header: string = "Color Tool";
+
+    public colors: string[] = [
+        "red", "white", "green", "orange", "blue"
+    ];
+
+    public newColor: string = "";
+
+    public addColor() {
+        this.colors.push(this.newColor);
+        this.newColor = "";
+    }
 
 }
