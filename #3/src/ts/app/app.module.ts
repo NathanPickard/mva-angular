6a567182775e58bd264d66b54fe20080d1e8f270
Nathan Pickard <nathan.pickard87@gmail.com>
2018-02-11 22:28:50 -0800

    Using a factory to create color service

--- #3/src/ts/app/app.module.ts
+++ #3/src/ts/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { Logger, simpleLogger } from "./services/logger";
-// import { Logger, simpleLogger } from "./services/logger";
 import { Colors } from "./services/colors";
 import { ColorsImmutable } from "./services/colors-immutable";
 import { AppComponent } from "./app.component";
@@ -10,12 +9,22 @@ import { AppComponent } from "./app.component";
 
 import "../../scss/styles.scss";
 
+const useImmutable = false;
+
+const colorsFactory = (logger: Logger) => {
+    if (useImmutable) {
+        return new ColorsImmutable(logger);
+    } else {
+        return new Colors(logger);
+    }
+};
+
 @NgModule({
     imports: [BrowserModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent],
     providers: [
         { provide: Logger, useValue: simpleLogger },
-        { provide: Colors, useClass: ColorsImmutable }]
+        { provide: Colors, useFactory: colorsFactory, deps: [Logger] }]
 })
 export class AppModule { }
